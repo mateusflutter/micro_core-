c159b46dd9712420d6d0762cc457f0b3d01d7bf2
mateusflutter <seu-email@gmail.com>
2025-02-01 20:07:23 -0300

    teste2

--- lib/micro_core_test.dart
+++ lib/micro_core_test.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:mini_app1/mini_app1.dart';
 
-void main() {
-  print('Iniciando micro-core');
-}
+void main() {}
 
 Widget returnMicroApp1() {
   return returnAll();
